887b3f1cb6548d8dd17473d0a7704cc68bc836ea
paty152007 <paty152007@gmail.com>
2026-04-02 11:23:36 +0200

    Add files via upload

---
+++ practica.py
@@ -0,0 +1,14 @@
+# Cree una clase de Python llamada Usuario que use el método init y cree un nombre de usuario y una contraseña. 
+# Crea un objeto usando la clase.
+
+class Usuario:
+    def __init__(self, usuario, password):
+        self.usuario = usuario
+        self.password = password
+
+    def mostrar_datos (self):
+        print(f"Tu usuario es {self.usuario}")
+
+usuario1 = Usuario("Patricia", "1234")
+
+usuario1.mostrar_datos()
